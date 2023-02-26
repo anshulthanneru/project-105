38ee203a960b6d25e14afcb3bf2d3554aab9aa60
anshulthanneru <82723623+anshulthanneru@users.noreply.github.com>
2023-02-26 14:45:28 -0700

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function take_snapshot() {
 };
 
 console.log("ml5 Version: ",ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/74JU7cN-E/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Bgg_Es2Bh/model.json',modelLoaded);
 
 function modelLoaded() {
     console.log("Model Loaded Successfully!");
